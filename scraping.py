ba714dd51cf5f396e4b8c21e686ee8adad9a7888
Pavel Tenenbaum <93852380+pavelt224@users.noreply.github.com>
2022-01-29 02:19:40 -0500

    Final Update

--- scraping.py
+++ scraping.py
@@ -21,7 +21,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "hemisphere": hemisphere(browser),
+        "hemispheres": hemisphere(browser),
         "last_modified": dt.datetime.now()
     }
 
